be03724c6f1a164f40df675344eaa0f1d1e57a3c
ARJUN 369 X <94111048+arjun111292@users.noreply.github.com>
2022-03-07 15:17:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TELEZEN/PIC_TESTER.git /PIC_TESTER
+  git clone https://github.com/arjun111292/VC_TESTER.git /VC_TESTER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PIC_TESTER
+  git clone $UPSTREAM_REPO /VC_TESTER
 fi
-cd /PIC_TESTER
+cd /VC_TESTER
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
